010e14e1d747f02ca74781cb87b675ff815092b7
Marius Wörfel <raborogit@gmail.com>
2022-02-02 11:20:56 +0100

    Delete tictactoe.py

--- socket project/socket project/tictactoe.py
+++
@@ -1,5 +0,0 @@
-class TicTacToe():
-    def __init__(self, client):
-        self.header = "[TTT]"
-        print(f"{self.header} Welcome to TicTacToe")
-        client.send(bytes("\n[TICTACTOE] is starting\n", "utf-8"))
