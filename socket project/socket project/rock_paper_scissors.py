5170cd1f4ee9dc3fc0ad2bef91c36db0317af52c
Marius Wörfel <raborogit@gmail.com>
2022-02-02 11:20:46 +0100

    Delete rock_paper_scissors.py

--- socket project/socket project/rock_paper_scissors.py
+++
@@ -1,5 +0,0 @@
-class RockPaperScissors():
-    def __init__(self, client):
-        self.header = "[RPS]"
-        print(f"{self.header} Welcome to RockPaperScissors")
-        client.send(bytes("\n[ROCKPAPERSCICCORS] is starting\n", "utf-8"))
