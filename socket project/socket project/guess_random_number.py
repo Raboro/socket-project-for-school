319a824677211f38973a9a48bde077d1fcd35ee0
Marius Wörfel <raborogit@gmail.com>
2022-02-02 11:19:50 +0100

    Delete guess_random_number.py

--- socket project/socket project/guess_random_number.py
+++
@@ -1,5 +0,0 @@
-class GuessRandomNumber():
-    def __init__(self, client):
-        self.header = "[GRN]"
-        print(f"{self.header} Welcome to GuessRandomNumber")
-        client.send(bytes("\n[GUESSRANDOMNUMBER] is starting\n", "utf-8"))
